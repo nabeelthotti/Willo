902483f1103dc80744bafe9574f3dcf65644d486
nabeelthotti <nabeel.hamza242@gmail.com>
2024-09-28 16:27:57 -0700

    Added hyperlink

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-// App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function Home() {
   const [showContact, setShowContact] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+  
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (showBackground) {
+      timerRef.current = setTimeout(() => {
+        setShowBackground(false);
+        timerRef.current = null; 
+      }, 5000);
+    }
+
+  
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+        timerRef.current = null;
+      }
+    };
+  }, [showBackground]);
+
+  const toggleBackground = () => {
+    if (showBackground) {
+      setShowBackground(false);
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+        timerRef.current = null;
+      }
+    } else {
+      setShowBackground(true);
+    }
+  };
+
+  const hideBackground = () => {
+    setShowBackground(false);
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Link to="/">
+        <a
+          href="https://www.instagram.com/willo_cc/?igsh=MzRlODBiNWFlZA%3D%3D"
+          onClick={hideBackground}
+        >
           <img
             src={`${process.env.PUBLIC_URL}/logo.png`}
             className="App-logo"
             alt="logo"
           />
-        </Link>
+        </a>
         <div className="contact-container">
           <span
             onClick={() => setShowContact(!showContact)}
@@ -31,12 +73,13 @@ function Home() {
         </div>
       </header>
 
-      <div className="App-background"></div>
+      <div className={`App-background ${showBackground ? "show" : ""}`}></div>
 
       <img
         src={`${process.env.PUBLIC_URL}/tree.png`}
         className="Tree-image"
         alt="Tree"
+        onClick={toggleBackground}
       />
 
       <footer className="App-footer">
